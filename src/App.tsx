4b170e5919ba5d235267904fc69faaf06ee9bc45
Renan Henrique <acidmud@gmail.com>
2021-08-19 14:32:09 -0300

    Add favicon && fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: red;
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>Hello World</Title>
     </div>
   );
 }
